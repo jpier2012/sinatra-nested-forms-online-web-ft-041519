cf754a99fc1d2043f114cb21d358944a8aa6527e
James Pier <jpier2012@gmail.com>
2019-05-30 14:32:40 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -4,9 +4,9 @@ class Ship
   attr_accessor :name, :type, :booty
 
   def initialize(params)
-    @name = params["name"]
-    @type = params["type"]
-    @booty = params["booty"]
+    @name = params[:name]
+    @type = params[:type]
+    @booty = params[:booty]
     @@all << self
   end
 
